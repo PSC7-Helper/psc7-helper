9d3b6c991c14db231610539c2d25c2fa48719e98
Florian Wehrhausen <fw-98@web.de>
2018-10-01 15:28:04 +0200

    edit footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -11,7 +11,10 @@
     <div class="text-center">
         <p>PSC7-Helper &copy; <?php
             echo date("Y");
-            ?> <br>Created by Florian Wehrhausen - teleropa GmbH</p>
+            ?> <br>Created by <a href="https://github.com/fwehrhausen" target="_blank">Florian Wehrhausen</a>,
+                              <a href="https://github.com/alea123" target="_blank">Waldemar Fraer</a> und 
+                              <a href="https://github.com/michael-rusch" target="_blank">Michael Rusch</a>
+        </p>
     </div>
 </footer>
 
