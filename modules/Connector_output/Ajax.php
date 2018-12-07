42d1f8adf49d88dc3cdc52e162f7600a38dd7e38
Rusch <michael@rusch.sh>
2018-12-07 20:44:44 +0100

    fix error: popen() has been disabled for security reasons

--- modules/Connector_output/Ajax.php
+++ modules/Connector_output/Ajax.php
@@ -18,62 +18,6 @@ use psc7helper\App\Connector\CommandHandler;
 class Ajax extends Ajax_Abstract implements Ajax_Interface {
 
     /**
-     * iframe
-     * @return string
-     */
-    public function iframe() {
-        $command = Session::get('cli_command');
-        $product = Session::get('cli_product');
-        $optionAll = Session::get('cli_option_all');
-        $optionVVV = Session::get('cli_option_vvv');
-        $optionBacklog = Session::get('cli_option_backlog');
-        $helper = new ConnectorHelper();
-        if (!Session::get('cli_command') || !$helper->checkCommand($command, $optionAll, $optionVVV, $optionBacklog)) {
-            $this->removeFromSession();
-            print '<pre>Command not allowed</pre>';
-            exit(1);
-        }
-        $cli = new CommandHandler();
-        if ($product) {
-            $cli->addCommand($command, $product)->prepareCommands();
-        } else {
-            $cli->addCommand($command)->prepareCommands();
-        }
-        if (!$cli->preparedCommands && $command != 'singlesync') {
-            $this->removeFromSession();
-            print '<pre>Command not found</pre>';
-            exit(1);
-        } elseif (!$cli->preparedCommands && $command == 'singlesync') {
-            $this->removeFromSession();
-            print '<pre>objectIdentifier not found for ' . $product . '</pre>';
-            exit(1);
-        }
-        print '<pre>';
-        while (@ob_end_flush());
-        $os = strtoupper(substr(PHP_OS, 0, 3));
-        foreach ($cli->preparedCommands as $cliCommand) {
-            if ($product) {
-                print 'ObjectIdentifier found for id ' . $product . PHP_EOL . PHP_EOL;
-            }
-            print 'Execute: ' . $cliCommand . PHP_EOL . PHP_EOL;
-            if ($os == 'WIN') {
-                $proc = popen("ping ::1 2>&1", 'r');
-            }
-            if ($os != 'WIN') {
-                $proc = popen("$cliCommand 2>&1", 'r');
-            }
-            while (!feof($proc)) {
-                print fread($proc, 4096);
-                @flush();
-            }
-            pclose($proc);
-        }
-        $this->removeFromSession();
-        print PHP_EOL . 'done';
-        print '</pre>';
-    }
-
-    /**
      * ajax
      * @return string
      */
@@ -87,7 +31,7 @@ class Ajax extends Ajax_Abstract implements Ajax_Interface {
         if (!Session::get('cli_command') || !$helper->checkCommand($command, $optionAll, $optionVVV, $optionBacklog)) {
             $this->removeFromSession();
             print '<pre>Command not allowed</pre>';
-            exit(0);
+            exit(1);
         }
         $cli = new CommandHandler();
         if ($product) {
@@ -104,28 +48,41 @@ class Ajax extends Ajax_Abstract implements Ajax_Interface {
             print '<pre>objectIdentifier not found for ' . $product . '</pre>';
             exit(1);
         }
-
-        while (@ob_end_flush());
         $os = strtoupper(substr(PHP_OS, 0, 3));
-        foreach ($cli->preparedCommands as $cliCommand) {
-            if ($product) {
-                print 'ObjectIdentifier found for id ' . $product . PHP_EOL . PHP_EOL;
-            }
-            print 'Execute: ' . $cliCommand . PHP_EOL . PHP_EOL;
-            if ($os == 'WIN') {
-                $proc = popen("ping ::1 2>&1", 'r');
-            }
-            if ($os != 'WIN') {
-                $proc = popen("$cliCommand 2>&1", 'r');
-            }
-            while (!feof($proc)) {
-                print fread($proc, 4096);
-                @flush();
-            }
-            pclose($proc);
+        if ($os == 'WIN') {
+            print 'Not supported on windows machine';
+            return false;
         }
-        $this->removeFromSession();
-        print PHP_EOL . 'done';
+        if (function_exists('popen') && is_callable('popen')) {
+            while (@ob_end_flush());
+            foreach ($cli->preparedCommands as $cliCommand) {
+                if ($product) {
+                    print 'ObjectIdentifier found for id ' . $product . PHP_EOL . PHP_EOL;
+                }
+                print 'Execute: ' . $cliCommand . PHP_EOL . PHP_EOL;
+                $proc = popen("$cliCommand 2>&1", 'r');
+                while (!feof($proc)) {
+                    print fread($proc, 4096);
+                    @flush();
+                }
+                pclose($proc);
+            }
+            $this->removeFromSession();
+            print PHP_EOL . 'done';
+        } else if (function_exists('shell_exec') && is_callable('shell_exec'))  {
+            foreach ($cli->preparedCommands as $cliCommand) {
+                if ($product) {
+                    print 'ObjectIdentifier found for id ' . $product . PHP_EOL . PHP_EOL;
+                }
+                print 'Execute: ' . $cliCommand . PHP_EOL . PHP_EOL;
+                $output = shell_exec("$cliCommand");
+                print $output;
+            }
+            $this->removeFromSession();
+            print PHP_EOL . 'done';
+        } else {
+            print 'php functions popen() and shell_exec() not allowd on your server';
+        }
     }
 
     /**
